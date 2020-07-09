f6ef8c5e591302539a69006db785f292c39df832
Manuel J. Gomez <manjgom@gmail.com>
2020-07-09 11:24:35 -0500

    Add: Initial menu build / define policies dictionary

--- covid_analysis.py
+++ covid_analysis.py
@@ -13,25 +13,39 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from dictionaries import states
 
-
-print(states)
-
 def look():
     print(covid_data.head())
     print(covid_data.shape)
     print(policy_data.head())
     print(policy_data.shape)
+def data_load():
+    covid_data = pd.read_csv('data/COVID Tracking - Historical.csv')
+    policy_data = pd.read_csv('data/Policy - Univ of Washington.csv')
     
+    covid_data.drop('DATA_SOURCE_NAME',axis=1,inplace=True)
+    covid_data.set_index('PROVINCE_STATE_CODE',inplace=True)
+    
+    policy_data.drop(['GLOBAL_BURDEN_DISEASE_IDENTIFIER',
+                     'PROVINCE_STATE_FIPS_NUMBER', 'PROVINCE_STATE_NAME'],
+                     axis=1, inplace=True)
+    
+    # get distinct policies from table and create dictionary for menu selection
+    policies_list = policy_data['POLICY_NAME'].unique()
+    policies_list.sort()
+    policies = {}
+    for i in range (1, len(policies_list)+1):
+        policies[i] = policies_list[i-1]
+    
+    
+   
 
+def interface():
+    print("-----------    Covid-19 Policy analysis tool   ----------------") 
+    print("- This utility facilitates analysis of government policy") 
+    print("- effects on Covid-19 cases.")
+
+
+data_load()
+interface()
 # data import and clean up
-covid_data = pd.read_csv('data/COVID Tracking - Historical.csv')
-policy_data = pd.read_csv('data/Policy - Univ of Washington.csv')
 
-covid_data.drop('DATA_SOURCE_NAME',axis=1,inplace=True)
-covid_data.set_index('PROVINCE_STATE_CODE',inplace=True)
-
-policy_data.drop(['GLOBAL_BURDEN_DISEASE_IDENTIFIER',
-                 'PROVINCE_STATE_FIPS_NUMBER', 'PROVINCE_STATE_NAME'],
-                 axis=1, inplace=True)
-
-look()
