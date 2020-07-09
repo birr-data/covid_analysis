ab6168504538d74438a01314be7cd1ee7930682e
Manuel J. Gomez <manjgom@gmail.com>
2020-07-09 10:34:19 -0500

    Add: Data clean up - column drop on policy table

--- covid_analysis.py
+++ covid_analysis.py
@@ -16,11 +16,22 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def look():
+    print(covid_data.head())
+    print(covid_data.shape)
+    print(policy_data.head())
+    print(policy_data.shape)
+    
+
+# data import and clean up
 covid_data = pd.read_csv('data/COVID Tracking - Historical.csv')
 policy_data = pd.read_csv('data/Policy - Univ of Washington.csv')
 
 covid_data.drop('DATA_SOURCE_NAME',axis=1,inplace=True)
 covid_data.set_index('PROVINCE_STATE_CODE',inplace=True)
 
-print(covid_data.head())
-print(covid_data.shape)
+policy_data.drop(['GLOBAL_BURDEN_DISEASE_IDENTIFIER',
+                 'PROVINCE_STATE_FIPS_NUMBER', 'PROVINCE_STATE_NAME'],
+                 axis=1, inplace=True)
+
+look()
